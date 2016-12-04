b9b81cf5554a97a183616e865d768649be536623
sujkid <suja.sreekumar@yahoo.com>
2016-12-04 17:25:23 -0500

    Bundle
    Added bundle and dependencies.js(???!!!) after grunt build

--- dependencies.js
+++ dependencies.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(36);
+	module.exports = __webpack_require__(37);
 
 
 /***/ },
@@ -10341,7 +10341,7 @@
 
 /***/ },
 
-/***/ 36:
+/***/ 37:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
