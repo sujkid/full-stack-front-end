a769a670fe5d3eb9b35ba7c780df17f6d0a12003
sujkid <suja.sreekumar@yahoo.com>
2016-12-05 00:25:05 -0500

    bundle

--- dependencies.js
+++ dependencies.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(37);
+	module.exports = __webpack_require__(39);
 
 
 /***/ },
@@ -10341,7 +10341,7 @@
 
 /***/ },
 
-/***/ 37:
+/***/ 39:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
